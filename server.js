1b0f2a1d3f0cd9d5ff00fe543b2ad3de17472cd6
Shan Basnayake <shanbasnayake@hotmail.com>
2024-04-14 12:28:39 +0530

    add db

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectiondb = require("./config/dbConnection");
 
 const app = express();
 dotenv.config();
+connectiondb();
 
 const port = process.env.PORT || 5000;
 
